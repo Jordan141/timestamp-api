d7b47f1398fbd5fe40ae2ec8d29927869bd34f8e
Jordan Moss <jmoss636@gmail.com>
2017-01-13 04:22:31 +0000

    Server code and html page

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 "use strict";
-var express = require('express');
-var PORT = process.env.PORT || 8080;
-var app = express();
+let express = require('express');
+let PORT = process.env.PORT || 8080;
+let app = express();
+let moment = require('moment');
+let fs = require('fs');
+let path = require('path');
 
-app.get('/',function(req,res){
-    res.send("Hello World!")
+app.listen(PORT, () => console.log("Server has started on port ", PORT))
+
+app.get('/',(req,res) => {
+    let fileName = path.join(__dirname, 'index.html')
+    res.sendFile(fileName, err => err ? res.status(err.status).end() : console.log('Sent: ', fileName))
 })
 
-app.listen(PORT, function(){
-    console.log("Server has started on port ", PORT);
-})
+
+
+app.get('/:datestring', (req,res) => {
+    
+    let myDate = (/^\d{8,}$/.test(req.params.datestring)) ? moment(req.params.datestring, "X") : moment(req.params.datestring, "MMMM D, YYYY");
+    
+    if(myDate.isValid()){
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+});
